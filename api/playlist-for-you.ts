8c5120c30a4d6dfc52403030dac5c9bdf3ea16ca
Mihreteab <mihreteabdemeke@gmail.com>
2024-05-23 23:11:35 +0300

    refactor: format files to imporve readablity

--- api/playlist-for-you.ts
+++ api/playlist-for-you.ts
@@ -1,4 +1,3 @@
-import { QueryClientProvider } from "@tanstack/react-query";
 
 export type MusicPlaylist = {
   collection_moto: string;
@@ -11,18 +10,18 @@ export type MusicPlaylist = {
   title: string;
   track_url: string;
 }
-  
 
-  export async function getPlaylistForYou() {
-    const res = await fetch('https://interview-ldfz.onrender.com/playlist-for-you');
-    const users = (await res.json()) as MusicPlaylist[];
-    return users;
-  }
-  
-  export async function  likePlaylistForYou(id: string) {
-    const requestOptions = {
-      method: 'PUT',
+
+export async function getPlaylistForYou() {
+  const res = await fetch('https://interview-ldfz.onrender.com/playlist-for-you');
+  const users = (await res.json()) as MusicPlaylist[];
+  return users;
+}
+
+export async function likePlaylistForYou(id: string) {
+  const requestOptions = {
+    method: 'PUT',
   };
-    const res = await fetch(`https://interview-ldfz.onrender.com/like-playlist-for-you/${id}`, requestOptions)
-    return res
-  }
+  const res = await fetch(`https://interview-ldfz.onrender.com/like-playlist-for-you/${id}`, requestOptions)
+  return res
+}
